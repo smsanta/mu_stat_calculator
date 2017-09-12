3faa1957acd559aba09967029b9ba2ca636f1ed3
Ignacio <santitax89@gmail.com>
2017-09-12 16:04:02 -0300

    Reupload the binary with the correct fix.

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 START /B /W %GRADLE_HOME%\bin\gradle build
-START /B /W xcopy build\distributions\mu_stat_calculator-0.9.0.zip releases\0.9.1\msc.zip /Y
+MD releases\0.9.2
+SLEEP 3
+START /B /W xcopy build\distributions\mu_stat_calculator-0.9.2.zip releases\0.9.2\msc.zip* /Y
 ECHO Release done!
